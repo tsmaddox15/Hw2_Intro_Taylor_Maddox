b13c5baaed201cc7a3cc193ef36115af3896ffc9
tsmaddox15 <tsmaddox15@gmail.com>
2018-10-09 14:01:46 -0400

    updates for redo.

--- app/src/main/java/com/example/tsmad_000/hw2_intro_taylor_maddox/Command.java
+++ app/src/main/java/com/example/tsmad_000/hw2_intro_taylor_maddox/Command.java
@@ -4,31 +4,67 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Command {
+    //List for current commands to be undone.
     LinkedList<Long> commandQ = new LinkedList<>();
+
+    //List for commands to be redone.
     LinkedList<Long> undone = new LinkedList<>();
 
+    //Used to allow a redo to happen can only happen in an undo has happened an no further
+    //changes to the clock hav been done
+    boolean allowRedo = false;
+
     int sizeQ = commandQ.size();
-    public void Command(){
+
+    public void Command() {
 
     }
+
+    /*
+    Called by all buttons to edit clock besides redo and undo.
+    If we were allowing redos, it sets that to false and clears the undone list.
+    This is done to properly determine when redo is allowed.
+    It then adds the command the user picked to the command list.
+     */
     public void Push(long lastCommand) {
+        if(allowRedo == true){
+            allowRedo = false;
+            undone.clear();
+        }
         commandQ.add(lastCommand);
     }
-    public long Undo(){
-        if(commandQ.isEmpty()){
+
+    /*
+    Called whenever undo is pressed
+    if empty return nothing. Allow redo since there is something to redo now.
+    returns the amount of milliseconds that was lasted added to the list.
+     */
+    public long Undo() {
+        if (commandQ.isEmpty()) {
             return 0;
-        }
-        else {
+        } else {
             long undo = commandQ.pollLast();
             undone.add(undo);
-//            undo = (-1)*undo;
+            allowRedo = true;
             return undo;
         }
 
     }
-    public long redo(){
-        long redo = undone.pollLast();
-        return redo;
+
+    /*
+    If allow redo is false or if list is empty return 0.
+    Else we get the last item in the list to do a redo.
+     */
+    public long Redo() {
+        if (allowRedo == false) {
+            return 0;
+        } else if (undone.isEmpty()) {
+            return 0;
+        } else {
+            long redo = undone.pollLast();
+            commandQ.add(redo);
+            return redo;
+        }
     }
 
 }
--- app/src/main/java/com/example/tsmad_000/hw2_intro_taylor_maddox/MainActivity.java
+++ app/src/main/java/com/example/tsmad_000/hw2_intro_taylor_maddox/MainActivity.java
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
 //    String dateString = sdf.format(date);
 //    TextClock dClock;
     TextView tClock;
-//    long date = System.currentTimeMillis();
+    //    long date = System.currentTimeMillis();
     long date = System.currentTimeMillis();
     SimpleDateFormat sdf = new SimpleDateFormat("MMM dd yyyy\nhh:mm:ss:a");
-    SimpleDateFormat sdfT = new SimpleDateFormat("hh:mm:ss:a");
-    SimpleDateFormat sdfD = new SimpleDateFormat("MMM dd yyyy");
+//    SimpleDateFormat sdfT = new SimpleDateFormat("hh:mm:ss:a");
+//    SimpleDateFormat sdfD = new SimpleDateFormat("MMM dd yyyy");
     String dateString = sdf.format(date);
-    ;
+
     //    String time;
     Command c;
     Button hourM;
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     Button monthM;
     Button monthP;
     Button undo;
+    Button redo;
 
 
     @Override
@@ -52,12 +53,13 @@ public class MainActivity extends AppCompatActivity {
         minM = (Button) findViewById(R.id.minuteM);
         minP = (Button) findViewById(R.id.minuteP);
         secM = (Button) findViewById(R.id.secM);
-        secP = (Button)findViewById(R.id.secPlus);
-        monthM = (Button)findViewById(R.id.monthM);
-        monthP = (Button)findViewById(R.id.monthP);
-        dayM = (Button)findViewById(R.id.dayMinus);
-        dayP = (Button)findViewById(R.id.dayPlus);
-        undo = (Button)findViewById(R.id.undo);
+        secP = (Button) findViewById(R.id.secPlus);
+        monthM = (Button) findViewById(R.id.monthM);
+        monthP = (Button) findViewById(R.id.monthP);
+        dayM = (Button) findViewById(R.id.dayMinus);
+        dayP = (Button) findViewById(R.id.dayPlus);
+        undo = (Button) findViewById(R.id.undo);
+        redo = (Button) findViewById(R.id.redo);
 
 
 //        dClock = (TextClock)findViewById(R.id.dClock1);
@@ -89,11 +91,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /*
+     Millisecond Conversions:
+     3600000 = a hour
+     60000 = a minute
+     5000 = 5 seconds
+     86400000 = a day
+     657000000*4 + 7200000 = A month
+     */
     public void changeTime(View v) {
+        //Used to get new time
         Calendar cal;
         Date newDate;
         Date timeBehind;
         String stringNewDate;
+        //Has a case for Plus(P), and Minus(M) for each button.
+        //Also includes redo and undo button.
         switch (v.getId()) {
             case R.id.hourM:
                 cal = Calendar.getInstance();
@@ -129,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
                 newDate.setTime(date);
                 cal.setTime(newDate);
                 cal.add(Calendar.MINUTE, -1);
-                date += -60000 ;
+                date += -60000;
                 timeBehind = cal.getTime();
                 stringNewDate = sdf.format(timeBehind);
                 Log.d("DateString: ", "String " + stringNewDate);
@@ -143,38 +156,41 @@ public class MainActivity extends AppCompatActivity {
                 newDate.setTime(date);
                 cal.setTime(newDate);
                 cal.add(Calendar.MINUTE, +1);
-                date += 60000 ;
+                date += 60000;
                 timeBehind = cal.getTime();
                 stringNewDate = sdf.format(timeBehind);
                 Log.d("DateString: ", "String " + stringNewDate);
                 tClock.setText(stringNewDate);
                 c.Push(60000);
                 break;
+
             case R.id.secM:
                 cal = Calendar.getInstance();
                 newDate = new Date();
                 newDate.setTime(date);
                 cal.setTime(newDate);
                 cal.add(Calendar.SECOND, -5);
-                date += -5000 ;
+                date += -5000;
                 timeBehind = cal.getTime();
                 stringNewDate = sdf.format(timeBehind);
                 tClock.setText(stringNewDate);
                 c.Push(-5000);
                 break;
+
             case R.id.secPlus:
                 cal = Calendar.getInstance();
                 newDate = new Date();
                 newDate.setTime(date);
                 cal.setTime(newDate);
                 cal.add(Calendar.SECOND, +5);
-                date += 5000 ;
+                date += 5000;
                 timeBehind = cal.getTime();
                 stringNewDate = sdf.format(timeBehind);
                 Log.d("DateString: ", "String " + stringNewDate);
                 tClock.setText(stringNewDate);
                 c.Push(5000);
                 break;
+
             case R.id.dayMinus:
                 cal = Calendar.getInstance();
                 newDate = new Date();
@@ -187,6 +203,7 @@ public class MainActivity extends AppCompatActivity {
                 tClock.setText(stringNewDate);
                 c.Push(-86400000);
                 break;
+
             case R.id.dayPlus:
                 cal = Calendar.getInstance();
                 newDate = new Date();
@@ -200,131 +217,232 @@ public class MainActivity extends AppCompatActivity {
                 tClock.setText(stringNewDate);
                 c.Push(86400000);
                 break;
+
             case R.id.monthM:
                 cal = Calendar.getInstance();
                 newDate = new Date();
                 newDate.setTime(date);
                 cal.setTime(newDate);
                 cal.add(Calendar.MONTH, -1);
-                date +=  -657000000;
-                date +=  -657000000;
-                date +=  -657000000;
-                date +=  -657000000;
-                date +=  -7200000;
+                date += -657000000;
+                date += -657000000;
+                date += -657000000;
+                date += -657000000;
+                date += -7200000;
                 timeBehind = cal.getTime();
                 stringNewDate = sdf.format(timeBehind);
                 Log.d("DateString: ", "String " + date);
                 tClock.setText(stringNewDate);
                 c.Push(-657000000);
                 break;
+
             case R.id.monthP:
                 cal = Calendar.getInstance();
                 newDate = new Date();
                 newDate.setTime(date);
                 cal.setTime(newDate);
                 cal.add(Calendar.MONTH, +1);
-                date +=  657000000;
-                date +=  657000000;
-                date +=  657000000;
-                date +=  657000000;
-                date +=  7200000;
+                date += 657000000;
+                date += 657000000;
+                date += 657000000;
+                date += 657000000;
+                date += 7200000;
                 timeBehind = cal.getTime();
                 stringNewDate = sdf.format(timeBehind);
                 Log.d("DateString: ", "String " + date);
                 tClock.setText(stringNewDate);
                 c.Push(657000000);
                 break;
-            case R.id.undo:
-           long undo = c.Undo();
-           if(undo == -3600000) {
-               cal = Calendar.getInstance();
-               newDate = new Date();
-               newDate.setTime(date);
-               cal.setTime(newDate);
-               cal.add(Calendar.HOUR, +1);
-               date += 3600000;
-           }
-           else if(undo == 3600000){
-               cal = Calendar.getInstance();
-               newDate = new Date();
-               newDate.setTime(date);
-               cal.setTime(newDate);
-               cal.add(Calendar.HOUR, -1);
-               date += -3600000;
-               }
-           else if(undo == -60000){
-               cal = Calendar.getInstance();
-               newDate = new Date();
-               newDate.setTime(date);
-               cal.setTime(newDate);
-               cal.add(Calendar.MINUTE, +1);
-               date += 60000;
-           }
-           else if(undo == 60000){
-               cal = Calendar.getInstance();
-               newDate = new Date();
-               newDate.setTime(date);
-               cal.setTime(newDate);
-               cal.add(Calendar.MINUTE, -1);
-               date += -60000;
-           }
-           else if(undo == -5000){
-               cal = Calendar.getInstance();
-               newDate = new Date();
-               newDate.setTime(date);
-               cal.setTime(newDate);
-               cal.add(Calendar.SECOND, +5);
-               date += 5000;
-           }
-           else if(undo == 5000){
-               cal = Calendar.getInstance();
-               newDate = new Date();
-               newDate.setTime(date);
-               cal.setTime(newDate);
-               cal.add(Calendar.SECOND, -5);
-               date += -5000;
-           }
-           else if(undo == -86400000){
-               cal = Calendar.getInstance();
-               newDate = new Date();
-               newDate.setTime(date);
-               cal.setTime(newDate);
-               cal.add(Calendar.DAY_OF_MONTH, +1);
-               date += 86400000;
-           }
-           else if(undo == 86400000){
-               cal = Calendar.getInstance();
-               newDate = new Date();
-               newDate.setTime(date);
-               cal.setTime(newDate);
-               cal.add(Calendar.DAY_OF_MONTH, -1);
-               date += -86400000;
-           }
-           else if(undo == -657000000){
-               cal = Calendar.getInstance();
-               newDate = new Date();
-               newDate.setTime(date);
-               cal.setTime(newDate);
-               cal.add(Calendar.MONTH, +1);
-               date += 657000000;
-               date += 657000000;
-               date += 657000000;
-               date += 657000000;
-               date +=  7200000;
-           }
-           else if(undo == 657000000){
-               cal = Calendar.getInstance();
-               newDate = new Date();
-               newDate.setTime(date);
-               cal.setTime(newDate);
-               cal.add(Calendar.MONTH, -1);
-               date += -657000000;
-               date += -657000000;
-               date += -657000000;
-               date += -657000000;
-               date +=  -7200000;
-           }
 
+                /*
+                Returns the milliseconds that were last added to a list maintained in Command.java
+                Based on that amount of time, goes through if,
+                else to do the opposite command to the clock.
+                Millisecond Conversions
+                3600000 = a hour
+                60000 = a minute
+                5000 = 5 seconds
+                86400000 = a day
+                657000000*4 + 7200000 = A month
+                 */
+            case R.id.undo:
+                long undo = c.Undo();
+                if (undo == -3600000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.HOUR, +1);
+                    date += 3600000;
+                } else if (undo == 3600000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.HOUR, -1);
+                    date += -3600000;
+                } else if (undo == -60000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.MINUTE, +1);
+                    date += 60000;
+                } else if (undo == 60000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.MINUTE, -1);
+                    date += -60000;
+                } else if (undo == -5000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.SECOND, +5);
+                    date += 5000;
+                } else if (undo == 5000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.SECOND, -5);
+                    date += -5000;
+                } else if (undo == -86400000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.DAY_OF_MONTH, +1);
+                    date += 86400000;
+                } else if (undo == 86400000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.DAY_OF_MONTH, -1);
+                    date += -86400000;
+                } else if (undo == -657000000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.MONTH, +1);
+                    date += 657000000;
+                    date += 657000000;
+                    date += 657000000;
+                    date += 657000000;
+                    date += 7200000;
+                } else if (undo == 657000000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.MONTH, -1);
+                    date += -657000000;
+                    date += -657000000;
+                    date += -657000000;
+                    date += -657000000;
+                    date += -7200000;
+
+                }
+                break;
+
+            /*
+                Returns the milliseconds that were last added to a list maintained in Command.java
+                Based on that amount of time, goes through if,
+                else to do the same command to the clock.
+                Millisecond Conversions:
+                3600000 = a hour
+                60000 = a minute
+                5000 = 5 seconds
+                86400000 = a day
+                657000000*4 + 7200000 = A month
+                 */
+            case R.id.redo:
+                long redo = c.Redo();
+                if (redo == -3600000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.HOUR, -1);
+                    date += -3600000;
+                }
+                else if (redo == 3600000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.HOUR, +1);
+                    date += 3600000;
+                } else if (redo == -60000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.MINUTE, -1);
+                    date += -60000;
+                } else if (redo == 60000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.MINUTE, +1);
+                    date += 60000;
+                } else if (redo == -5000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.SECOND, -5);
+                    date += -5000;
+                } else if (redo == 5000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.SECOND, 5);
+                    date += 5000;
+                } else if (redo == -86400000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.DAY_OF_MONTH, -1);
+                    date += -86400000;
+                } else if (redo == 86400000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.DAY_OF_MONTH, +1);
+                    date += 86400000;
+                } else if (redo == -657000000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.MONTH, -1);
+                    date += -657000000;
+                    date += -657000000;
+                    date += -657000000;
+                    date += -657000000;
+                    date += -7200000;
+                } else if (redo == 657000000) {
+                    cal = Calendar.getInstance();
+                    newDate = new Date();
+                    newDate.setTime(date);
+                    cal.setTime(newDate);
+                    cal.add(Calendar.MONTH, +1);
+                    date += 657000000;
+                    date += 657000000;
+                    date += 657000000;
+                    date += 657000000;
+                    date += 7200000;
+                }
+                break;
         }
     }
 }
